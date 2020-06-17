ba0b2b773616bfbb53047c02e1c811d8221e33f0
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-06-16 22:13:22 -0500

    Creando aplicacion todo redux

--- README.md
+++ README.md
@@ -4,6 +4,27 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Development server
 
+| Funcionalidades |
+| ------------- |
+
+|Tocaremos temas como:|
+|Creación de componentes|
+|Modelos|
+|Acciones|
+|Reducers|
+|Estados|
+|Estado inicial|
+|Agregar tareas mediante acciones|
+|Payload|
+|Editar tareas|
+|Despliegues a GitHub y GitHub Pages|
+|Pero uno de los temas principales, es aclarar cómo romper la relación que tiene JavaScript con los objetos y que no sean procesados por referencia, si no que sean creados objetos ||nuevos. Y de esa manera siempre regresar nuevos estados.|
+|https://github.com/Klerith/TODO-CSS-Template|
+|http://todomvc.com/|
+|ng g m todos/todo --flat => sirve para que lo cree en la carpeta de module|
+|ng g c footer --skipTests|
+
+
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## Code scaffolding
---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,49 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear, toggle, editar } from './todo.actions';
+import { Todo } from './models/todo.model';
+
+export const estadoInicial : Todo[] = [
+  new Todo('Salvar al mundo'),
+  new Todo('Ser el mejor desarrollador de Sur America'),
+  new Todo('Superar a todos los desarrolladores de digitalware'),
+  new Todo('Ser el mas teso en ingles'),
+  new Todo('Ser super duro desarrollando todo tipo de aplicaciones'),
+];
+
+const _todoReducer = createReducer(estadoInicial,
+  on(crear, (state,{ texto}) => [...state, new Todo( texto)]),
+  on(toggle, (state,{ id}) => {
+    
+    return state.map( todo => {
+      
+      if( todo.id === id ){
+        return{
+          ...todo,
+          completado: !todo.completado
+        }
+      } else {
+        return todo;
+      }
+
+    });
+  }),
+  on(editar, (state,{id, texto}) => {
+    
+    return state.map( todo => {
+      
+      if( todo.id === id ){
+        return{
+          ...todo,
+          texto: texto
+        }
+      } else {
+        return todo;
+      }
+
+    });
+  }),
+);
+
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+}
